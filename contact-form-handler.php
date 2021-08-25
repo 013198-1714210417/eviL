649abc460f9815149339ebcfadb73722ad1ac6ef
013198-1714210417 <89550892+013198-1714210417@users.noreply.github.com>
2021-08-25 15:51:18 -0700

    1

---
+++ contact-form-handler.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['messsage'];
+
+$email_from = '013198_1714210417@protonmail.com';
+
+$email_subject = "new request";
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                  "User Message: $message.\n";
+
+$to = "013198_1714210417@protonmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index.html");
+
+?>
